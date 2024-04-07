37d3e94acf8b6a848a0996792745c99ce3a803e4
fernando stubing <fstubing@yahoo.com>
2024-04-07 14:22:09 -0400

    agregando componentes bootstrap y detalles finales

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+// lógica de tooltip bootstrap
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+
+// lógica popover bootstrap
+const popoverTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="popover"]'
+);
+const popoverList = [...popoverTriggerList].map(
+  (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+);
